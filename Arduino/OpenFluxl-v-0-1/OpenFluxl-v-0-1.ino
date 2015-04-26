8d43470d62021fc8c3f18504f4b12573de179d1c
PieterVanBoheemen <pieter@vanboheemen.nl>
2015-04-26 15:55:05 +0200

    added moveDrop function and serial control
    Added ability to control individual fluxls over serial

--- Arduino/OpenFluxl-v-0-1/OpenFluxl-v-0-1.ino
+++ Arduino/OpenFluxl-v-0-1/OpenFluxl-v-0-1.ino
@@ -23,8 +23,12 @@ PID myPID(&Input, &Output, &Setpoint, .8, .1, .01, DIRECT);
 
 // Vars
 int targetVoltage = 100;
+int numFluxls = 6;
 String buffer;
+
+// Pins
 int DriverPin = 3;
+int FluxlPins[(numFluxls -1)] = {7,8,9,10,11,12};
 
 void setup(){
   
@@ -32,8 +36,10 @@ void setup(){
   Serial.begin(115200);
 
   // Set Pin Modes
-  pinMode(11, OUTPUT); // OCR2B 
-  pinMode(DriverPin, OUTPUT); // OCR2A  
+  for(i=0;i<numFluxls;i++) {
+    pinMode(FluxlPins[i],OUTPUT);
+  }
+  pinMode(DriverPin, OUTPUT); // Mosfet Driver (OCR2A)
   
   // Change PWM frequency
   pwmSetup(); // set pwm to 31 KHz
@@ -62,19 +68,31 @@ void loop(){
         Serial.print("id:OpenFluxl "); // device identifier
         Serial.print(FluxlVersion); 
       } else if (buffer.startsWith("vol")) {
+        // Change target voltage
         targetVoltage = buffer.substring(3).toInt();
         
         // Sanity check
         if(targetVoltage < 0) targetVoltage = 0;
-        if(targetVoltage > 300) targetVoltage = 300;
+        else if(targetVoltage > 300) targetVoltage = 300;
+        else targetVoltage = 0;
         
         // Report through serial
-        Serial.print(F("Setting target voltage to "));
+        Serial.print(F("Setting target voltage to: "));
         Serial.print(targetVoltage);
         Serial.println(F(" V"));
+      } else if (buffer.startsWith("fluxl")) {
+        // move droplet to new fluxl
+        int tmpInt = buffer.substring(3).toInt();
+        
+        // Sanity check
+        if(tmpInt < 0) tmpInt = 0;
+        else if(tmpInt > 5) tmpInt = 5;
+        else tmpInt = 0; 
+        
+        moveDrop(tmpInt);
       }
       else {
-        Serial.println("Unknown cmd.");
+        Serial.println(F("Unknown cmd."));
       }
       buffer="";
     } else buffer+=c;
@@ -113,20 +131,33 @@ void PidUpdate() {
   
 }
 
+void moveDrop(int TargetFluxl) {
+  // Open the circuit through the target Fluxl
+  for(i=0;i<numFluxls;i++) {
+    if(i==TargetFluxl) {
+      digitalWrite(FluxlPins[i],HIGH);
+      
+      // Report through serial
+      Serial.print(F("Moving droplet to Fluxl: "));
+      Serial.println(i);      
+    }
+    else {
+      digitalWrite(FluxlPins[i],LOW);
+    }
+  }
+}
+
 void pwmSetup(){
   
-  /* Derived from: http://letsmakerobots.com/content/changing-pwm-frequencies-arduino-controllers */
+/* Derived from: http://letsmakerobots.com/content/changing-pwm-frequencies-arduino-controllers */
 //---------------------------------------------- Set PWM frequency for D5 & D6 -------------------------------
-  
 //TCCR0B = TCCR0B & B11111000 | B00000001;    // set timer 0 divisor to     1 for PWM frequency of 62500.00 Hz
 //TCCR0B = TCCR0B & B11111000 | B00000010;    // set timer 0 divisor to     8 for PWM frequency of  7812.50 Hz
 TCCR0B = TCCR0B & B11111000 | B00000011;    // set timer 0 divisor to    64 for PWM frequency of   976.56 Hz
 //TCCR0B = TCCR0B & B11111000 | B00000100;    // set timer 0 divisor to   256 for PWM frequency of   244.14 Hz
 //TCCR0B = TCCR0B & B11111000 | B00000101;    // set timer 0 divisor to  1024 for PWM frequency of    61.04 Hz
 
-
 //---------------------------------------------- Set PWM frequency for D9 & D10 ------------------------------
-  
 //TCCR1B = TCCR1B & B11111000 | B00000001;    // set timer 1 divisor to     1 for PWM frequency of 31372.55 Hz
 //TCCR1B = TCCR1B & B11111000 | B00000010;    // set timer 1 divisor to     8 for PWM frequency of  3921.16 Hz
 TCCR1B = TCCR1B & B11111000 | B00000011;    // set timer 1 divisor to    64 for PWM frequency of   490.20 Hz
@@ -134,7 +165,6 @@ TCCR1B = TCCR1B & B11111000 | B00000011;    // set timer 1 divisor to    64 for 
 //TCCR1B = TCCR1B & B11111000 | B00000101;    // set timer 1 divisor to  1024 for PWM frequency of    30.64 Hz
 
 //---------------------------------------------- Set PWM frequency for D3 & D11 ------------------------------
-  
 TCCR2B = TCCR2B & B11111000 | B00000001;    // set timer 2 divisor to     1 for PWM frequency of 31372.55 Hz
 //TCCR2B = TCCR2B & B11111000 | B00000010;    // set timer 2 divisor to     8 for PWM frequency of  3921.16 Hz
 //TCCR2B = TCCR2B & B11111000 | B00000011;    // set timer 2 divisor to    32 for PWM frequency of   980.39 Hz
